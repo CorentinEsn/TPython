a82a43afbbcc9219056510a0b26d6072ce657f82
Killian Jaeger <904438@edu.p.lodz.pl>
2023-12-01 12:15:30 +0100

    commit de kiki

--- TP 2 Tkinter/Calculatrice.py
+++ TP 2 Tkinter/Calculatrice.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import math
 
 class CalculatorApp:
     def __init__(self, master):
@@ -8,31 +9,60 @@ class CalculatorApp:
         self.display_var = tk.StringVar()
         self.display_var.set("")
 
+        self.scientific_mode = False
+
         self.create_widgets()
+        self.create_menu()
 
     def create_widgets(self):
         # Affichage
-        display = tk.Entry(self.master, textvariable=self.display_var, justify="right", font=('Helvetica', 16))
+        display = tk.Entry(self.master, textvariable=self.display_var, justify="right", font=('Helvetica', 16), state='readonly')
         display.grid(row=0, column=0, columnspan=4, sticky="nsew")
 
         # Boutons
-        buttons = [
-            '7', '8', '9', '/',
-            '4', '5', '6', '*',
-            '1', '2', '3', '-',
-            '0', 'C', '=', '+'
+        self.button_layout = [
+            ['7', '8', '9', '/','sin'],
+            ['4', '5', '6', '*','cos'],
+            ['1', '2', '3', '-','tan'],
+            ['0', 'C', 'AC', '+','sqrt'],
+            ['=','(',')','^']
         ]
 
-        row_val = 1
-        col_val = 0
+        self.create_buttons()
 
-        for button in buttons:
-            tk.Button(self.master, text=button, padx=20, pady=20, command=lambda b=button: self.on_button_click(b)).grid(
-                row=row_val, column=col_val)
-            col_val += 1
-            if col_val > 3:
-                col_val = 0
-                row_val += 1
+    def create_menu(self):
+        menu_bar = tk.Menu(self.master)
+        self.master.config(menu=menu_bar)
+
+        mode_menu = tk.Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="Mode", menu=mode_menu)
+
+        mode_menu.add_command(label="Standard", command=self.set_standard_mode)
+        mode_menu.add_command(label="Scientifique", command=self.set_scientific_mode)
+
+    def set_standard_mode(self):
+        self.scientific_mode = False
+        self.clear_and_create_buttons()
+
+    def set_scientific_mode(self):
+        self.scientific_mode = True
+        self.clear_and_create_buttons()
+
+    def clear_and_create_buttons(self):
+        # Efface les boutons existants
+        for widget in self.master.winfo_children():
+            if isinstance(widget, tk.Button):
+                widget.destroy()
+
+        # Crée les nouveaux boutons
+        for i, row in enumerate(self.button_layout):
+            for j, button in enumerate(row):
+                if self.scientific_mode or button not in ['sqrt', '^', 'sin', 'cos', 'tan', '(', ')']:
+                    tk.Button(self.master, text=button, padx=20, pady=20,
+                              command=lambda b=button: self.on_button_click(b)).grid(row=i+1, column=j, sticky="nsew")
+
+    def create_buttons(self):
+        self.clear_and_create_buttons()
 
     def on_button_click(self, button):
         current_display = self.display_var.get()
@@ -41,6 +71,10 @@ class CalculatorApp:
             try:
                 result = str(eval(current_display))
                 self.display_var.set(result)
+            except ZeroDivisionError:
+                self.display_var.set("Error: Division by zero")
+            except ValueError:
+                self.display_var.set("Error: Invalid input")
             except Exception as e:
                 self.display_var.set("Error")
 
@@ -50,6 +84,20 @@ class CalculatorApp:
         elif button == 'AC':
             self.display_var.set("")
 
+        elif button == 'sqrt':
+            try:
+                result = str(math.sqrt(float(current_display)))
+                self.display_var.set(result)
+            except ValueError:
+                self.display_var.set("Error: Invalid input")
+
+        elif self.scientific_mode:
+            if button == '^':
+                self.display_var.set(current_display + '**')
+            elif button in ['sin', 'cos', 'tan']:
+                self.display_var.set(button + '(' + current_display + ')')
+            else:
+                self.display_var.set(current_display + button)
         else:
             self.display_var.set(current_display + button)
 
