eea751cf1186eda049b1f2bad34662686bf4aa35
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2023-12-01 12:14:39 +0100

    Resize de la fenêtre

--- TP 2 Tkinter/Calculatrice.py
+++ TP 2 Tkinter/Calculatrice.py
@@ -8,6 +8,8 @@ class CalculatorApp:
         self.display_var = tk.StringVar()
         self.display_var.set("")
 
+        self.col = 3
+        self.row = 0
         self.create_widgets()
 
     def create_widgets(self):
@@ -15,24 +17,33 @@ class CalculatorApp:
         display = tk.Entry(self.master, textvariable=self.display_var, justify="right", font=('Helvetica', 16))
         display.grid(row=0, column=0, columnspan=4, sticky="nsew")
 
+
+
         # Boutons
         buttons = [
             '7', '8', '9', '/',
             '4', '5', '6', '*',
             '1', '2', '3', '-',
-            '0', 'C', '=', '+'
+            '', '0', '=', '+',
+            '', '','AC' ,'C'
         ]
 
         row_val = 1
         col_val = 0
 
         for button in buttons:
-            tk.Button(self.master, text=button, padx=20, pady=20, command=lambda b=button: self.on_button_click(b)).grid(
-                row=row_val, column=col_val)
+            if button == '':
+                tk.Button(self.master, state="disabled", padx=20, pady=20).grid(row=row_val, column=col_val, sticky="nsew")
+            else:
+                tk.Button(self.master, text=button, padx=20, pady=20, command=lambda b=button: self.on_button_click(b)).grid(
+                    row=row_val, column=col_val, sticky="nsew")
             col_val += 1
-            if col_val > 3:
+            if col_val > self.col:
                 col_val = 0
                 row_val += 1
+        self.row = row_val
+
+
 
     def on_button_click(self, button):
         current_display = self.display_var.get()
@@ -56,4 +67,8 @@ class CalculatorApp:
 if __name__ == "__main__":
     root = tk.Tk()
     app = CalculatorApp(root)
+    for i in range(app.row):
+        root.rowconfigure(i, weight=1)
+    for i in range(app.col+1):
+        root.columnconfigure(i, weight=1)
     root.mainloop()
