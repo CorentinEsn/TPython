c4e4c737c75526a707f57eb87d7d7de748957acf
Killian Jaeger <904438@edu.p.lodz.pl>
2023-12-05 14:35:58 +0100

    ajout exceptions sur le main TP1

--- TP 1/main.py
+++ TP 1/main.py
@@ -1,49 +1,64 @@
 from Date import Date
 from Etudiant import Etudiant
 
-
 def HelloWorld():
-    print("Bonjour le monde !")
-
+    try:
+        print("Bonjour le monde !")
+    except Exception as e:
+        print(f"Une erreur s'est produite : {e}")
 
 def OpenAndAppendFile():
+    try:
+        # Choix du fichier à ouvrir
+        print("Choisissez un fichier à ouvrir :")
+        filename = input("> ")
+        with open(filename, "at") as fic:
+            # Ajout du texte
+            print("Texte à ajouter :")
+            text = input("> ")
+            fic.write(text + "/n")
 
-    # Choix du fichier à ouvrir
-    print("Choisissez un fichier à ouvrir :")
-    filename = input("> ")
-    fic = open(filename, "at")
+        # Affichage du contenu du fichier
+        print("Le fichier contient le texte suivant :")
+        with open(filename, "r") as fic:
+            print(fic.read())
 
-    # Ajout du texte
-    print("Texte à ajouter :")
-    text = input("> ")
-    fic.write(text + "/n")
-    fic.close()
+        # Vider le fichier
+        with open(filename, "wt"):
+            pass
 
-    # Affichage du contenu du fichier
-    print("Le fichier contient le texte suivant :")
-    fic = open(filename, "r")
-    print(fic.read())
-    fic.close()
+    except FileNotFoundError:
+        print(f"Le fichier '{filename}' n'a pas été trouvé.")
+    except Exception as e:
+        print(f"Une erreur s'est produite : {e}")
+    finally:
+        print("Opération terminée.")
 
-    # Vider le fichier
-    fic = open(filename, "wt")
-    fic.close()
-
-
-# Liste des étudiants depuis un fichier CSV
 def NewEtudiantsFromCSV(filename):
     etudiants = []  # Liste d'étudiants
-    fic = open(filename, "r")   # Ouverture du fichier
-    for line in fic:    # Lecture du fichier ligne par ligne
-        line = line.strip()
-        line = line.split(";")
-        etudiants.append(Etudiant(line[0], line[1], Date((line[2]))))
-    fic.close()
+    try:
+        with open(filename, "r") as fic:
+            # Lecture du fichier ligne par ligne
+            for line in fic:
+                line = line.strip()
+                line = line.split(";")
+                assert len(line) == 3, "Format de ligne invalide"
+                etudiants.append(Etudiant(line[0], line[1], Date(line[2])))
+    except FileNotFoundError:
+        print(f"Le fichier '{filename}' n'a pas été trouvé.")
+    except AssertionError as ae:
+        print(f"Erreur de format dans le fichier CSV : {ae}")
+    except Exception as e:
+        print(f"Une erreur s'est produite : {e}")
+    finally:
+        print("Opération terminée.")
     return etudiants
 
-
-# Construction de la liste et affichage des étudiants
-Etudiants = NewEtudiantsFromCSV("TP 1/fichetu.csv")
-for etudiant in Etudiants:
-    print(etudiant)
-
+try:
+    HelloWorld()
+    #OpenAndAppendFile()
+    Etudiants = NewEtudiantsFromCSV("fichetu.csv")
+    for etudiant in Etudiants:
+        print(etudiant)
+except Exception as e:
+    print(f"Une erreur s'est produite : {e}")
