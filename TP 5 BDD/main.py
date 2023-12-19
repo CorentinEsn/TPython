2964a29106f9438de4dd3b4fc0666121d45de3bf
Killian Jaeger <904438@edu.p.lodz.pl>
2023-12-19 17:34:12 +0100

    TODO: Mise en place d'une liste de liste pour récupérer une table via un fichier csv + Refonte des fichiers csv

--- TP 5 BDD/main.py
+++ TP 5 BDD/main.py
@@ -5,4 +5,21 @@ import pandas as pd
 def SQLConnect():
     sql.connect()
 
-def readTable(path): #reads a .csv file using pandas and returns the table
+
+def readTable(path):
+    try:
+        # Essayez avec l'encodage latin-1
+        df_csv = pd.read_csv(path, sep=';', encoding='latin-1', header=None, skiprows=0)
+
+        # Convertir le DataFrame en une liste de listes
+        tableau_de_tableaux = df_csv.values.tolist() if not df_csv.empty else None
+        return tableau_de_tableaux
+    except pd.errors.EmptyDataError:
+        # Le fichier est vide
+        return None
+    except Exception as e:
+        print(f"Erreur lors de la lecture du fichier CSV : {e}")
+        return None
+
+# Exemple d'utilisation
+file = readTable("data_Base_de_données/regions.csv")
