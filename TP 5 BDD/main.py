359033d0c2f1e11dc4f8620e2d0f3630f8604427
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2023-12-19 17:36:31 +0100

    TP5 SQLConnectAndInsert

--- TP 5 BDD/main.py
+++ TP 5 BDD/main.py
@@ -1,8 +1,27 @@
 import sqlite3 as sql
 import pandas as pd
 
-# Create a SQL connection to our SQLite database
-def SQLConnect():
-    sql.connect()
 
-def readTable(path): #reads a .csv file using pandas and returns the table
+# Create a SQL connection to our SQLite database
+def SQLConnectAndInsert(name, table):
+    connect = sql.connect(name + ".db")
+    cur = connect.cursor()
+
+    # check if table already exists, else create it
+    res = cur.execute("SELECT name FROM sqlite_master WHERE name='" + name + "'")
+    res = res.fetchone()
+    if res is None:
+        print(name + " table does not exist")
+        create = input("Do you want to create it ? Y/N : ")
+        if create == "Y" or create == "y":
+            attributes = str(table[0])[1:-1]  # return list content without []
+            cur.execute("CREATE TABLE " + name + "(" + attributes + ")")
+        else:
+            return -1
+    # insert the data
+    for line in table[1:]:
+        cur.execute("INSERT INTO " +name+" VALUES ("+str(line)[1:-1]+")")
+    cur.commit()
+
+def readTable(path):  # reads a .csv file using pandas and returns the table
+    return
