a2dde8e3e1d129bbeed94d66b1b5d93da5835e1d
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2024-01-12 09:38:43 +0100

    TP8 fourmi algorithm

--- TP 8 Fourmis/fourmis.py
+++ TP 8 Fourmis/fourmis.py
@@ -1,17 +1,28 @@
 class Fourmi:
-    def __init__(self, x, y, direction, color):
+    def __init__(self, x, y, direction, color, size):
         self.x = x
         self.y = y
         self.direction = direction
         self.color = color
+        self.size = size
 
     def move(self):
-        pass
+        self.turn()
+        self.forward()
+        self.paint()
 
     def turn(self):
+        # explore le voisinage
+        # calcul de la distace aux couleurs voisines
+        # choix de la couleur la plus proche
+        # calcul de la direction
         pass
 
     def paint(self):
+        # si petite fourmi, peint la case
+        # si grande fourmi, peint la case et les cases voisines
         pass
 
-
+    def forward(self):
+        # avance d'une case dans la direction
+        pass
