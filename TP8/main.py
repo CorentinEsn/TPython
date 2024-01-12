ce68b1f8cc452a7e33e9fc6c9c3e239fdf3e7f0a
Killian Jaeger <904438@edu.p.lodz.pl>
2024-01-12 09:17:53 +0100

    Début TP8

---
+++ TP8/main.py
@@ -0,0 +1,75 @@
+import threading
+import psutil
+import multiprocessing
+def calcul_long():
+    n = 1E7
+    while n>0:
+        n -= 1
+
+def print_cpu_usage():
+    # Utilisation du CPU par coeur
+    cpu_percent_per_core = psutil.cpu_percent(percpu=True)
+    for i, core_percent in enumerate(cpu_percent_per_core):
+        print(f"Core {i + 1} Usage: {core_percent}%")
+
+    # Charge moyenne du systeme au cours des 1, 5 et 15 dernieres minutes
+    load_average = psutil.getloadavg()
+    print(f"Load Average (1min, 5min, 15min): {load_average}")
+
+    # Utilisation globale du CPU
+    overall_cpu_percent = psutil.cpu_percent()
+    print(f"Overall CPU Usage: {overall_cpu_percent}%")
+
+def use_processes():
+    if __name__ == '__main__':
+        # Creation de deux processus
+        print ("------------- Processus ----------------")
+        print("\n")
+        process1 = multiprocessing.Process(target=calcul_long)
+        process2 = multiprocessing.Process(target=calcul_long)
+        process3 = multiprocessing.Process(target=calcul_long)
+        process4 = multiprocessing.Process(target=calcul_long)
+
+        # Demarrage des processus
+        process1.start()
+        process2.start()
+        process3.start()
+        process4.start()
+
+        # Afficher l'utilisation du CPU pendant que les processus s'executent
+        print_cpu_usage()
+        print("-----------------------------------")
+        print("\n")
+        # Attendre que les processus se terminent
+        process1.join()
+        process2.join()
+        process3.join()
+        process4.join()
+def use_threads():
+    print("------------- Threads ----------------")
+    print("\n")
+    # Creation de deux threads
+    thread1 = threading.Thread(target=calcul_long)
+    thread2 = threading.Thread(target=calcul_long)
+    thread3 = threading.Thread(target=calcul_long)
+    thread4 = threading.Thread(target=calcul_long)
+
+    # Demarrage des threads
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread4.start()
+
+    # Afficher l'utilisation du CPU pendant que les threads s'executent
+    print_cpu_usage()
+    print("----------------------------------")
+    print("\n")
+    # Attendre que les threads se terminent
+    thread1.join()
+    thread2.join()
+    thread3.join()
+    thread4.join()
+
+
+#use_threads()
+use_processes()
